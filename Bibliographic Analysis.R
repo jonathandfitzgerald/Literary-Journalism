25567088484beae0f956ee5f2f89721b1cdcdfa2
jonathandfitzgerald <jonathanfitzgerald@me.com>
2015-11-17 13:15:37 -0500

    Add Bibliography CSV to output.

--- Bibliographic Analysis.R
+++ Bibliographic Analysis.R
@@ -2,7 +2,7 @@ library(dplyr)
 library(ggplot2)
 library(tidyr)
 
-#Read in txt file and separate each line of Sims Bibliography
+#Read in txt file and separate each line of Sims Bibliography (http://normansims.com/wp-content/uploads/2014/05/Bibliography-of-Literary-Journalism-Scholarship-and-Articles.pdf)
 SimsBib = scan("data/SimsBib.txt", character(0), sep = "\n")
 SimsBibDF = SimsBib %>% data.frame()
 SimsBibDF = SimsBibDF %>% mutate(year=gsub(".*(\\d{4}).*","\\1",.)) 
@@ -11,7 +11,7 @@ SimsBibDF = SimsBibDF %>% mutate(author=gsub("(^[A-Za-z]{1,20}).*","\\1",.))
 SimsBibDF = SimsBibDF %>% unite(id, author, year, sep = "_")
 
 
-#Read in txt file and separate each line of Bibliography
+#Read in txt file and separate each line of Bibliography (https://www.dropbox.com/s/3a1wilg8lxngag5/LJS_Biblio_v120301.pdf?dl=0)
 LJBib = scan("data/LJSBib.txt", character(0), sep = "\n")
 LJBibDF = LJBib %>% data.frame()
 LJBibDF = LJBibDF %>% mutate(year=gsub(".*(\\d{4}).*","\\1",.)) 
@@ -19,7 +19,8 @@ LJBibDF = LJBibDF %>% mutate("year" = as.numeric(year))
 LJBibDF = LJBibDF %>% mutate(author=gsub("(^[A-Za-z]{1,20}).*","\\1",.))
 LJBibDF = LJBibDF %>% unite(id, author, year, sep = "_")
 
-#Read in txt file and separate each line of LJS Bibliography (Fall 2011)
+
+#Read in txt file and separate each line of LJS Bibliography (Fall 2011) (http://ialjs.journalism.ryerson.ca/wp-content/uploads/2012/01/125-127_SelectedBibliographyMaguire-2.pdf)
 LJBib2 = scan("data/LJSBib2.txt", character(0), sep = "\n")
 LJBib2DF = LJBib2 %>% data.frame()
 LJBib2DF = LJBib2DF %>% mutate(year=gsub(".*(\\d{4}).*","\\1",.)) 
@@ -27,10 +28,13 @@ LJBib2DF = LJBib2DF %>% mutate("year" = as.numeric(year))
 LJBib2DF = LJBib2DF %>% mutate(author=gsub("(^[A-Za-z]{1,20}).*","\\1",.))
 LJBib2DF = LJBib2DF %>% unite(id, author, year, sep = "_")
 
+
+
 #Combine Sims and LJS Bibs
 allBib <- rbind(LJBibDF, SimsBibDF, LJBib2DF)
 
 allBibClean = distinct(allBib, id)
+write.csv(allBibClean, file=paste('output/LJBibliography.csv',sep=""))
 
 #Visualzing Critical texts by year
 allBibViz = allBibClean %>% 
